18e4b1cdd8d70adb60ff122b2d00c7d3ea875b99
gonyango <grantononyango@gmail.com>
2025-05-11 19:05:49 +0300

    Add main.py to implement port scanner with ASCII art banner

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+
+# Generate and display an ASCII art banner for the port scanner
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
