9d4bef384a36a4a5116a52d81081cf014fe3e1ef
gonyango <grantononyango@gmail.com>
2025-05-11 19:15:06 +0300

    Refactor port scanning logic and enhance output messages

--- main.py
+++ main.py
@@ -14,3 +14,22 @@ if len(sys.argv) == 2:
 else:
     # Exit the program with an error message if no argument is provided
     sys.exit("Program needs an IP ADDR argument \npython main.py 0.0.0.0")
+
+# Display a message indicating the start of the scanning process with the target and current timestamp
+print("Scanning {} started at: {}".format(target, datetime.now()))
+
+try:
+    # Iterate through all possible ports (1 to 65534)
+    for port in range(1, 65535):
+        # Create a socket object for IPv4 and TCP connections
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Set a timeout for the connection attempt
+        socket.setdefaulttimeout(1)
+
+        # Attempt to connect to the target on the specified port
+        result = s.connect_ex((target, port))
+        # If the connection is successful (result is 0), the port is open
+        if result == 0:
+            print("Port {} is open".format(port))
+        # Close the socket after checking the port
+        s.close()
